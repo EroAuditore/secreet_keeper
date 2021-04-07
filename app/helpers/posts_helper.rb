f363ab93cc8776a00376ffd473c8a2a8d81380b4
Chukwuma Obasi <ccobasi8@gmail.com>
2021-04-07 22:07:35 +0100

    Remove logic from views

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,15 @@
 module PostsHelper
+    def is_signed_in(post)
+        if user_signed_in?
+            render partial: 'authors'
+        end
+    end
+
+    def user_is_signed_in(post)
+        if user_signed_in? && current_user.id == post.user_id
+            render partial: 'feed'
+        
+        end
+
+    end
 end
