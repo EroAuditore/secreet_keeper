3f3e4531ddc6594e365c7103f6617ed74ca91836
Chukwuma Obasi <ccobasi8@gmail.com>
2021-04-07 22:54:17 +0100

    Fix linters error

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,15 +1,9 @@
 module PostsHelper
-    def is_signed_in(post)
-        if user_signed_in?
-            render partial: 'authors'
-        end
-    end
+  def his_signed_in(_post)
+    render partial: 'authors' if user_signed_in?
+  end
 
-    def user_is_signed_in(post)
-        if user_signed_in? && current_user.id == post.user_id
-            render partial: 'feed'
-        
-        end
-
-    end
+  def user_is_signed_in(post)
+    render partial: 'feed' if user_signed_in? && current_user.id == post.user_id
+  end
 end
